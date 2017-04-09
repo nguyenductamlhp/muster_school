916d035f39326ed2d31106b29661ef241d16b1bf
nguyenductamlhp <nguyenductamlhp@gmail.com>
2017-04-09 15:26:48 -0700

    [ADD] Add check login method

--- MusterApp/src/com/model/service/UserService.java
+++ MusterApp/src/com/model/service/UserService.java
@@ -1,5 +1,15 @@
 package com.model.service;
 
+import java.sql.ResultSet;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
+
+import com.model.entity.User;
+
 public class UserService {
 
 	public UserService() {
@@ -8,6 +18,16 @@ public class UserService {
 	
 	public boolean isLoginSuccess(String username, String password) {
 		boolean isSuccess = false;
+		EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("MusterApp");
+		EntityManager entityManager = entityManagerFactory.createEntityManager();
+		String hsql = "select * from User where username = :uname and password = :pass";
+		Query query = entityManager.createQuery(hsql);
+		query.setParameter(0, username);
+		query.setParameter(1, password);
+		List<User> list_user = query.getResultList();
+		if (list_user.size() == 1) {
+			isSuccess = true;
+		}
 		return isSuccess;
 	}
 }
