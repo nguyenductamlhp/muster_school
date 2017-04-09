130d01b6b550f0d25d6d7b2a0ee5572411634711
nguyenductamlhp <nguyenductamlhp@gmail.com>
2017-04-09 15:14:56 -0700

    [ADD] Config persistence.xml

---
+++ MusterApp/src/com/model/service/UserService.java
@@ -0,0 +1,13 @@
+package com.model.service;
+
+public class UserService {
+
+	public UserService() {
+
+	}
+	
+	public boolean isLoginSuccess(String username, String password) {
+		boolean isSuccess = false;
+		return isSuccess;
+	}
+}
