13bca1dbf5d718f71a231e54bf143b88765ce6c0
nguyenductamlhp <nguyenductamlhp@gmail.com>
2017-04-10 20:58:33 -0700

    [ADD] Add getAllUser()

--- MusterApp/src/com/model/service/UserService.java
+++ MusterApp/src/com/model/service/UserService.java
@@ -1,6 +1,7 @@
 package com.model.service;
 
 import java.sql.ResultSet;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.EntityManager;
@@ -16,11 +17,23 @@ public class UserService {
 
 	}
 	
+	public List<User> getAllUser() {
+		List<User> listAllUser = new ArrayList<>();
+		EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("MusterApp");
+		EntityManager entityManager = entityManagerFactory.createEntityManager();
+		String hsql = "select u from User u";
+		Query query = entityManager.createQuery(hsql);
+		listAllUser = query.getResultList();
+		entityManager.close();
+		entityManagerFactory.close();
+		return listAllUser;
+	}
+	
 	public boolean isLoginSuccess(String username, String password) {
 		boolean isSuccess = false;
 		EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("MusterApp");
 		EntityManager entityManager = entityManagerFactory.createEntityManager();
-		String hsql = "select * from User where username = :uname and password = :pass";
+		String hsql = "select u from User u where username = :uname and password = :pass";
 		Query query = entityManager.createQuery(hsql);
 		query.setParameter(0, username);
 		query.setParameter(1, password);
@@ -28,6 +41,8 @@ public class UserService {
 		if (list_user.size() == 1) {
 			isSuccess = true;
 		}
+		entityManager.close();
+		entityManagerFactory.close();
 		return isSuccess;
 	}
 }
